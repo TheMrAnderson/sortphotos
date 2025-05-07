65f352e630fe88d378e03ef01a9f74906ccb9fbf
Matt Anderson <mpanderson06@fastmail.com>
2025-05-07 13:29:37 -0500

    Dockerizing the script and loading it to a cron job

--- src/sortphotos.sh
+++ src/sortphotos.sh
@@ -1,6 +1,6 @@
 SEARCH_DIR=$1
 DEST_DIR=$2
-REPO_DIR=$3
+SCRIPT_DIR=$3
 
 clear
 # Remove empty files
@@ -9,8 +9,9 @@ find $SEARCH_DIR -type f -empty -delete
 
 # Remove json files
 echo Removing json files
-python3 $REPO_DIR/ScriptFiles/Python/removefiles.py $SEARCH_DIR json
+python3 $SCRIPT_DIR/removefiles.py $SEARCH_DIR json
 
 # Sort photos
 echo Sorting photos
-python3 $REPO_DIR/SortPhotos/src/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
+python3 $SCRIPT_DIR/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
+mkdir -p $SEARCH_DIR
