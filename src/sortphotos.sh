b99ecae39e4ac010b2ee2f3b9b169d75379ec011
Matt Anderson <mpanderson06@fastmail.com>
2025-05-17 07:15:37 -0500

    Adding additional logging

--- src/sortphotos.sh
+++ src/sortphotos.sh
@@ -2,6 +2,7 @@ SEARCH_DIR=$1
 DEST_DIR=$2
 SCRIPT_DIR=$3
 
+echo "$(date) Starting to sort photos in $SEARCH_DIR and move them to $DEST_DIR."
 clear
 # Remove empty files
 echo Removing all empty files
@@ -9,9 +10,11 @@ find $SEARCH_DIR -type f -empty -delete
 
 # Remove json files
 echo Removing json files
-python3 $SCRIPT_DIR/removefiles.py $SEARCH_DIR json
+/usr/bin/python3 $SCRIPT_DIR/removefiles.py $SEARCH_DIR json
 
 # Sort photos
 echo Sorting photos
-python3 $SCRIPT_DIR/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
+/usr/bin/python3 $SCRIPT_DIR/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
 mkdir -p $SEARCH_DIR
+echo "$(date) Finished sorting photos."
+echo ""
