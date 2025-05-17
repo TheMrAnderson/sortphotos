5a4a946bc51afd603cad6ed51f9ba3f301ecbeca
Matt Anderson <mpanderson06@fastmail.com>
2025-05-17 08:36:40 -0500

    remoing trhe explicit path

--- src/sortphotos.sh
+++ src/sortphotos.sh
@@ -10,11 +10,11 @@ find $SEARCH_DIR -type f -empty -delete
 
 # Remove json files
 echo Removing json files
-/usr/bin/python3 $SCRIPT_DIR/removefiles.py $SEARCH_DIR json
+python3 $SCRIPT_DIR/removefiles.py $SEARCH_DIR json
 
 # Sort photos
 echo Sorting photos
-/usr/bin/python3 $SCRIPT_DIR/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
+python3 $SCRIPT_DIR/sortphotos.py $SEARCH_DIR $DEST_DIR --recursive --sort %Y/%Y-%m/ --rename %Y%m%d_%H%M%S%f --clean_src_dir True
 mkdir -p $SEARCH_DIR
 echo "$(date) Finished sorting photos."
 echo ""
