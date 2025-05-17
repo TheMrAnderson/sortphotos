e949350486f29a3df8d16443d3fcb049fbdfd61a
Matt Anderson <mpanderson06@fastmail.com>
2025-05-17 09:36:06 -0500

    python3 is not in the PATH, setting it

--- src/sortphotos.sh
+++ src/sortphotos.sh
@@ -2,6 +2,7 @@ SEARCH_DIR=$1
 DEST_DIR=$2
 SCRIPT_DIR=$3
 
+export PATH="/usr/local/bin:/usr/bin:/bin"
 echo "$(date) Starting to sort photos in $SEARCH_DIR and move them to $DEST_DIR."
 clear
 # Remove empty files
